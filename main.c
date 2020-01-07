8a659a200339fa652eba2a00c457725eb2824a88
Dovran Annakuliev <55741979+Dovran-Annakuliev@users.noreply.github.com>
2020-01-07 19:15:54 +0300

    mg

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rfork <rfork@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/05 15:13:18 by rfork             #+#    #+#             */
-/*   Updated: 2020/01/07 18:20:33 by rfork            ###   ########.fr       */
+/*   Updated: 2020/01/07 18:24:57 by rfork            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,45 @@ void	suicide(char *error_text)
 	exit(0);
 }
 
+void	dda(t_mlx *data)
+{
+	t_point *p1;
+	t_point *p2;
+	​
+	p1 = (t_point *)malloc(sizeof(t_point));
+	p2 = (t_point *)malloc(sizeof(t_point));
+	​
+	p1->x = 0;
+	p1->y = 0;
+	p2->x = 320;
+	p2->y = 240;
+	​
+	double dx = p2->x - p1->x;
+	double dy = p2->y - p1->y;
+	​
+	double steps = fabs(dx) > fabs(dy) ? dx : dy;
+	​
+	double Xinc = dx / steps;
+	double Yinc = dy / steps;
+	​
+	double X = p1->x;
+	double Y = p1->y;
+	for (int i = 0; i <= steps; i++)
+	{
+		mlx_pixel_put(data->mlx_ptr, data->win_ptr, (int)round(X), (int)round(Y), 16711680);
+		X += Xinc;
+		Y += Yinc;
+	}
+}
+​
 int		main(void)
 {
 	t_mlx *data;
-
+	​
 	data = start();
 	mlx_key_hook(data->win_ptr, deal_key, data);
+	dda(data);
+	​
 	mlx_loop(data->mlx_ptr);
 	return (0);
 }
