43290576e2d20e883ef9ff0e8f77a1f5556d87ee
Dovran Annakuliev <55741979+Dovran-Annakuliev@users.noreply.github.com>
2020-03-19 15:42:54 +0300

    mg

--- libft/get_next_line.c
+++ libft/get_next_line.c
@@ -35,7 +35,8 @@ static int	ft_save(char **line, char **s_buf)
 
 static int	errors(const int fd)
 {
-	if (fd < 0 || fd > OPEN_MAX || BUFF_SIZE == 0)
+	//if (fd < 0 || fd > OPEN_MAX || BUFF_SIZE == 0)
+	if (fd < 0 || fd > 10240 || BUFF_SIZE == 0)
 		return (1);
 	return (0);
 }
@@ -43,7 +44,8 @@ static int	errors(const int fd)
 int			get_next_line(const int fd, char **line)
 {
 	char		buf[BUFF_SIZE + 1];
-	static char	*s_buf[OPEN_MAX];
+	//static char	*s_buf[OPEN_MAX];
+	static char	*s_buf[10240];
 	int			ret;
 	char		*temp;
 
